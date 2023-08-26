c2d8ebe2c1564d965f45bcd3c483994b3b6d01b4
DoganGumustekin <91798233+DoganGumustekin@users.noreply.github.com>
2023-08-27 00:39:38 +0300

    27.08.2023 Add(Update-Delete) Add(Notification)

--- TrialProjectMVC/Controllers/PhonexController.cs
+++ TrialProjectMVC/Controllers/PhonexController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using TrialProjectMVC.DBContext;
 using TrialProjectMVC.Models;
 
@@ -13,6 +14,7 @@ namespace TrialProjectMVC.Controllers
             _context = context;
         }
 
+        #region Ekleme
         public IActionResult Index()
         {
             List<PhoneModel> phoneModels = _context.Phone.ToList();
@@ -20,16 +22,101 @@ namespace TrialProjectMVC.Controllers
         }
 
         public IActionResult PhoneAdd()
-        { 
+        {
             return View();
         }
 
         [HttpPost]
         public IActionResult PhoneAdd(PhoneModel obj)
         {
-            _context.Phone.Add(obj);
+            if (obj.Phone == obj.Address)
+            {
+                ModelState.AddModelError("Address", "Adres alanı ile telefon alanı birbirine eşit olamaz...");
+            }
+            if (!obj.Address.IsNullOrEmpty() || !obj.Phone.IsNullOrEmpty())
+            {
+                if (obj.Address.ToLower() == "test")
+                {
+
+                    ModelState.AddModelError("", "Test yanlış bir değer");
+                }
+
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Phone.Add(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Yeni Kayıt Eklendi";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        #endregion
+
+        #region Güncelleme
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["error"] = "Kayıt Bulunamadı...";
+                return NotFound();
+            }
+            PhoneModel phoneModel = _context.Phone.Find(id);
+            if (phoneModel == null)
+            {
+                TempData["error"] = "Kayıt Bulunamadı...";
+                return NotFound();
+            }
+            return View(phoneModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(PhoneModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Phone.Update(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Kayıt Güncellendi";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        #endregion
+
+        #region Silme
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["error"] = "Kayıt Bulunamadı...";
+                return NotFound();
+            }
+            PhoneModel phoneModel = _context.Phone.Find(id);
+            if (phoneModel == null)
+            {
+                TempData["error"] = "Kayıt Bulunamadı...";
+                return NotFound();
+            }
+            return View(phoneModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            PhoneModel? phoneModel = _context.Phone.Find(id);
+            if (phoneModel == null)
+            {
+                TempData["error"] = "Kayıt Bulunamadı...";
+                return NotFound();
+            }
+            _context.Remove(phoneModel);
             _context.SaveChanges();
+            TempData["success"] = "Kayıt Silindi";
             return RedirectToAction("Index");
         }
+        #endregion
+
     }
 }
--- TrialProjectMVC/Models/PhoneModel.cs
+++ TrialProjectMVC/Models/PhoneModel.cs
@@ -1,12 +1,20 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TrialProjectMVC.Models
 {
     public class PhoneModel
     {
         public int PhoneId { get; set; }
+
+
+        [Required(ErrorMessage ="Bu alanın doldurulması zorunludur.")]
         [DisplayName("Telefon Numarası")]
+        [MaxLength(20)]
         public string Phone { get; set; }
+
+
+        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur.")]
         [DisplayName("Adres")]
         public string Address { get; set; }
     }
